05152f7c96c0fc6066645fd2595574bd5364ae88
Xiaoying-Huang <hxybecomexs@gmail.com>
2023-06-24 15:28:25 +0100

    insertion sort

---
+++ FreeCodeCamp/ImplementInsertionSort.js
@@ -0,0 +1,30 @@
+// Implement Insertion Sort
+// The next sorting method we'll look at is insertion sort. This method works by building up a sorted array at the beginning of the list. It begins the sorted array with the first element. Then it inspects the next element and swaps it backwards into the sorted array until it is in sorted position. It continues iterating through the list and swapping new items backwards into the sorted portion until it reaches the end. This algorithm has quadratic time complexity in the average and worst cases.
+
+// Instructions: Write a function insertionSort which takes an array of integers as input and returns an array of these integers in sorted order from least to greatest.
+
+
+function insertionSort(array) {
+    // Only change code below this line
+    function swap(arr, a, b) {
+        let tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+    for (let i = 0; i < array.length - 1; i++) {
+        if (array[i] > array[i + 1]) {
+            swap(array, i, i + 1);
+            for (let j = i; j > 0; j--) {
+                if (array[j] < array[j - 1]) {
+                    swap(array, j, j - 1);
+                }
+            }
+        }
+    }
+    return array;
+    // Only change code above this line
+}
+
+//test
+let a = insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]);
+console.log(a);
